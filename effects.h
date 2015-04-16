2cf723b85363d0232035f220d2119ad5847e0e6c
kredal <kredal@gmail.com>
2015-04-16 05:05:44 -0400

    Update effects.h
    Started working on Game of Life mode.  Because that's what you do with a matrix.

--- effects.h
+++ effects.h
@@ -313,3 +313,35 @@ void hearts() {
      FastLED.clear();
   y++;
 }
+
+
+
+//Run Conway's Game of Life on the glasses
+void gameoflife() {
+// startup tasks
+if (effectInit == false) {
+effectInit = true;
+effectDelay = 20;
+//create buffer and randomize start screen
+const uint8_t buffer[] = {
+68, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 69,
+29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14,
+30, 31, 32, 33, 34, 35, 36, 70, 71, 37, 38, 39, 40, 41, 42, 43,
+57, 56, 55, 54, 53, 52, 51, 72, 73, 50, 49, 48, 47, 46, 45, 44,
+74, 58, 59, 60, 61, 62, 75, 76, 77, 78, 63, 64, 65, 66, 67, 79
+};
+for (byte x = 0; x < kMatrixWidth; x++) {
+for (byte y = 0; y < kMatrixHeight; y++) {
+  if (random(0, 10) > 4) buffer[XY(x,y)] = 1;
+  else buffer[XY(x,y)] = 0;
+}
+
+//determine neighbors and life.  The nature of this is that the first frame won't be seen. That's ok.
+for (byte x = 0; x < kMatrixWidth; x++) {
+for (byte y = 0; y < kMatrixHeight; y++) {
+  
+  
+leds[XY(x,y)] = CHSV(cycleHue, 255, brightness);
+}
+}
+}
